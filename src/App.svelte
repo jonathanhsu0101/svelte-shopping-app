501d8985a238a866e7996de9d1d460b9a86d39d6
jonathanhsu0101 <jonathan.hsu0101@gmail.com>
2019-06-08 23:53:18 -0600

    rebuild

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Product from "./Product.svelte";
+
+  let title = "";
+  let price = 0;
+  let description = "";
+
+  function setTitle(event) {
+    title = event.target.value;
+  }
+</script>
+
+<style>
+  section {
+    width: 30rem;
+    margin: auto;
+  }
+
+  label,
+  input,
+  textarea {
+    width: 100%;
+  }
+</style>
+
+<section>
+  <div>
+    <label for="title">Title</label>
+    <input type="text" id="title" value={title} on:input="{setTitle}" />
+  </div>
+  <div>
+    <label for="price">Price</label>
+    <input type="number" id="price" bind:value={price} />
+  </div>
+  <div>
+    <label for="description">Description</label>
+    <textarea rows="3" id="description" bind:value={description} />
+  </div>
+</section>
+
+<Product />
---
+++ src/Product.svelte
@@ -0,0 +1,52 @@
+<script>
+  let productTitle;
+  let productDescription;
+  let productPrice;
+</script>
+
+
+<style>
+  div {
+    margin: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+    border-radius: 5px;
+    background: white;
+  }
+
+  h1 {
+    font-size: 1.25rem;
+    margin: 0.25rem 0;
+    color: #4b4b4b;
+  }
+
+  h2 {
+    font-size: 1rem;
+    margin: 0.25rem 0;
+  }
+
+  p {
+    margin: 0.25rem 0;
+  }
+
+  button {
+    font: inherit;
+    padding: 0.15rem 0.5rem;
+    background: #d10057;
+    border: 1px solid #d10057;
+    color: white;
+    cursor: pointer;
+  }
+
+  button:hover,
+  button:active {
+    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.26);
+  }
+</style>
+
+<div>
+  <h1>TITLE</h1>
+  <h2>PRICE</h2>
+  <p>DESCRIPTION</p>
+  <button>Add to Cart</button>
+</div>
